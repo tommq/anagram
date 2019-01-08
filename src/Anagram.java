c4d6cebb5b14bb774941fa02ad419e23d2358e8a
tomek <tomtomm77@gmail.com>
2019-01-08 15:58:19 +0100

    2

--- src/Anagram.java
+++ src/Anagram.java
@@ -16,22 +16,17 @@ public class Anagram {
         desiredHashes.add("e4820b45d2277f3844eac66c903e84be");
         desiredHashes.add("23170acc097c24edb98fc5488ab033fe");
         desiredHashes.add("665e5bcb0c20062fe8abaaf4628bb154");
-
+        String anagram = "poultry outwits ants".replaceAll(" ", "");
+        
         MessageDigest MD5 = getMD5();
 
-        List<String> inputList = new ArrayList<String>();
         File inputF = new File("wordlist");
 
         InputStream inputFS = new FileInputStream(inputF);
 
         BufferedReader br = new BufferedReader(new InputStreamReader(inputFS));
-
-        // skip the header of the csv
-
-        inputList = br.lines().collect(Collectors.toList());
+        desiredHashes.addAll(br.lines().collect(Collectors.toList()));
         br.close();
-        desiredHashes.addAll(inputList);
-        System.out.println("inputList = " + inputList.size());
 
 
 
